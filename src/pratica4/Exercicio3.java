5371e47e3fcb20a1eaf9f8daf68f668d6008c702
gabriel <gabriel.f.espanhol@hotmail.com>
2023-12-11 22:08:42 -0300

    Saídas dos dados formatadas

--- src/pratica4/Exercicio3.java
+++ src/pratica4/Exercicio3.java
@@ -4,25 +4,25 @@ public class Exercicio3 {
 
 	public static void main(String[] args) {
 		int dpri = 0, dsec = 0;
-		int[][] mat = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }
-		};
+		int[][] mat = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
+		System.out.println("Elementos da Diagonal Principal: ");
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				if (j == i) {
 					dpri += mat[i][j];
-					System.out.println("Elementos da Diagonal Principal: " + mat[i][j]);
+					System.out.print(mat[i][j]+" ");
 				}
 			}
 		}
-
+		System.out.println("\nElementos da Diagonal Secundária: ");
 		for (int i = 0; i < 3; i++) {
-			System.out.println("Elementos da Diagonal Secundária: " + mat[i][2 - i]);
+			System.out.print(mat[i][2 - i]+" ");
 			dsec += mat[i][2 - i];
 		}
 
-		System.out.println("Soma dos Elementos da Diagonal Principal: " + dpri);
-		System.out.println("Soma dos Elementos da Diagonal Secundária: " + dsec);
+		System.out.println("\nSoma dos Elementos da Diagonal Principal: \n" + dpri);
+		System.out.println("Soma dos Elementos da Diagonal Secundária: \n" + dsec);
 
 	}
 
